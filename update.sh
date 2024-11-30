5aadf2e87287c10093acd51888899a9b8073aca0
aldwin-m <moutarlier.al@gmail.com>
2024-11-30 14:10:22 +0100

    update new version of the blog

--- update.sh
+++ update.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-file="./versions"
-VERSION=$(cat "$file")
-echo "update new version of the blog - $VERSION"
-
 rsync -av --delete "/home/corvus/Documents/Obsidian Vault/posts" "/home/corvus/corvusblog/content/"
 python3 images.py
 git add .
-git commit -m "update new version of the blog - $VERSION"
+git commit -m "update new version of the blog"
 git push
