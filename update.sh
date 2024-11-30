26f60e9f0f47f3fe32da23ad353e287ebcf04e46
aldwin-m <moutarlier.al@gmail.com>
2024-11-30 22:47:27 +0100

    update new version - 30/11/2024 22:47:27

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+dt=$(date '+%d/%m/%Y %H:%M:%S');
 rsync -av --delete "/home/corvus/corvusblog/corvusblog-obsidian/posts" "/home/corvus/corvusblog/content/"
 python3 images.py
 git add .
-git commit -m "update new version of the blog"
+git commit -m "update new version - $dt"
 git push
