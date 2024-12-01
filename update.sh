64bc0b94960a5306f194b1abab32dc94d532b496
aldwin-m <moutarlier.al@gmail.com>
2024-12-01 10:18:45 +0100

    update new version - 01/12/2024 10:18:45

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dt=$(date '+%d/%m/%Y %H:%M:%S');
 python3 images.py
-git add .
-git commit -m "update new version - $dt"
-git push
+git add . > /dev/null 2>&1
+git commit -m "update new version - $dt" > /dev/null 2>&1
+git push > /dev/null 2>&1
